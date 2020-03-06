8a3f4dbb0de006b3b27e1464206c35c793d97f2d
jordan laidig <jrlaidig@gmail.com>
2020-03-06 15:50:01 -0800

    added some starting scripts

---
+++ Quibits/Assets/Scripts/Block.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+enum BlockTypes
+{
+    Orange,
+    Red,
+    Yellow,
+    White,
+    Green, 
+    Blue, 
+    Black, 
+    Purple
+};
+
+public class Block : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
